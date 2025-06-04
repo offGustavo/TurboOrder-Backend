1cd1ebb27b1776d202391cd6907b8a06293eff53
AnnielyBorges <neykhenney@gmail.com>
2025-06-04 12:46:11 -0300

    Implementado sistema de logout

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
+
+import homeRoutes from "./routes/home.js"
 import cardapioRoutes from "./routes/cardapio.js";
 import produtosRoutes from "./routes/produtos.js";
 import clienteRoutes from "./routes/cliente.js";
@@ -10,7 +12,6 @@ import pedidoRoutes from "./routes/pedido.js";
 import statusRoutes from "./routes/status.js";
 import registerRoutes from "./routes/register.js";
 import loginRoutes from "./routes/login.js"
-import homeRoutes from "./routes/home.js"
 
 const app = express();
 const PORT = 8800;
@@ -24,6 +25,7 @@ app.use(cors({
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.use('/', homeRoutes);
 app.use("/clientes", clienteRoutes);
 app.use("/cardapio", cardapioRoutes);
 app.use("/produtos", produtosRoutes);
@@ -31,7 +33,6 @@ app.use("/pedidos", pedidoRoutes);
 app.use('/status', statusRoutes);
 app.use('/cadastro', registerRoutes);
 app.use('/login', loginRoutes);
-app.use('/', homeRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,16 @@
 import express from "express";
 import { verifyUser } from "../controllers/home.js";
+import cookieParser from "cookie-parser";
 
 const router = express.Router();
 
 router.get("/", verifyUser, (req, res) => {
     return res.json({ Status: "Success", username: req.username });
+});
+
+router.get("/logout", (req, res) => {
+    res.clearCookie('token');
+    return res.json({ Status: "Success" });
 });
 
 export default router;
