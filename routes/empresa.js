7fa98c2c268a41d2d0cf85b53c2777ad9a8d1904
Luiz Gustavo <125596752+offGustavo@users.noreply.github.com>
2025-06-17 11:47:53 -0300

    ADD: delete empresa

--- routes/empresa.js
+++ routes/empresa.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { createEmpresa } from "../controllers/empresa.js";
+import { createEmpresa, getEmpresas, getEmpresaById, deleteEmpresa } from "../controllers/empresa.js";
 
 const router = express.Router();
 
 router.post("/", createEmpresa);
+router.get("/", getEmpresas);
+router.get("/:id", getEmpresaById);
+router.patch("/:id", deleteEmpresa)
 
 export default router;
+
+
