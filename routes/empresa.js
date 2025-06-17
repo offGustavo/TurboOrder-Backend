6611369b62c25cb1da0f97bebd85d72af07e9ff6
Luiz Gustavo <125596752+offGustavo@users.noreply.github.com>
2025-06-17 14:57:07 -0300

    ADD: add edicao a empresa

--- routes/empresa.js
+++ routes/empresa.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createEmpresa, getEmpresas, getEmpresaById, deleteEmpresa } from "../controllers/empresa.js";
+import { createEmpresa, getEmpresas, getEmpresaById, deleteEmpresa, editEmpresaById } from "../controllers/empresa.js";
 
 const router = express.Router();
 
@@ -7,6 +7,7 @@ router.post("/", createEmpresa);
 router.get("/", getEmpresas);
 router.get("/:id", getEmpresaById);
 router.patch("/:id", deleteEmpresa)
+router.put("/:id", editEmpresaById);
 
 export default router;
 
