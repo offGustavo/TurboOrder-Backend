a9d5a3a32ea086148263875d636e291375d5266d
AnnielyBorges <neykhenney@gmail.com>
2025-06-24 00:03:51 -0300

    cleanup: remoção da página de cadastro obsoleta

--- routes/register.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { addUser, verifyCode } from "../controllers/register.js";
-
-const router = express.Router();
-
-router.post("/", addUser);
-router.post("/verify", verifyCode);
-
-export default router;
